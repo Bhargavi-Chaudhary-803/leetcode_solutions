2b0278ffd9d848d824afde5823b56add09d7940e
BHARGAVI CHAUDHARY <bhargavichaudhary803@gmail.com>
2026-03-27 01:17:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        int i;
-        vector<int> firstHalf(n);
-        vector<int> secondHalf(n);
+    int i;
 
-        for(i=0;i<2*n;i++){
-            if(i<n){
-                firstHalf[i] = nums[i];
-            }
-            else{
-                secondHalf[i-n] = nums[i];
-            }
-        }
+    vector<int> indice;
+    for(i = 0; i < n ; i++){
+        indice.push_back(nums[i]);
+        indice.push_back(nums[i+n]);
+        
+    }
 
-        vector<int> result;
-        for(i=0;i<n;i++){
-            
-            result.push_back(firstHalf[i]);
-            result.push_back(secondHalf[i]);
-        }
-       return result;
+    return indice;
     }
 };
