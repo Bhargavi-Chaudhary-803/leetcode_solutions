6e33d9f54873deaf2ed2ada80847e0974658a82d
BHARGAVI CHAUDHARY <bhargavichaudhary803@gmail.com>
2026-03-31 00:39:15 +0530

    Time: 2 ms (78.95%), Space: 7.7 MB (87.64%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
-    int fib(int n) {
-        if (n <= 1)
-        return n;
-    return fib(n - 1) + fib(n - 2);
+    int fib(int n){
+        int i, n1 = 0, n2 = 1;
+        int num;
+        if (n < 2) {
+            return n;
+        } 
+            
+        for (i = 2; i <= n; i++) {
+    
+            num = n1 + n2;
+            n1 = n2;
+            n2 = num;
+        }
+        return num;    
     }
 };
