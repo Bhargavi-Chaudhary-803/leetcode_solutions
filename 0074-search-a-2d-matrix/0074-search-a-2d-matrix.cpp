e46ce2d98934fa8cde057e5be642cd46ed53f2f3
BHARGAVI CHAUDHARY <bhargavichaudhary803@gmail.com>
2026-04-04 23:59:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,9 +3,10 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int i,j;
         int m = matrix.size();
-        int n = matrix[i].size();
+        
         for(i=0;i<m;i++){
-            for(j=0;j<n;j++){
+            int n = matrix[i].size();
+              for(j=0;j<n;j++){
                 if(matrix[i][j] == target){
                     return true;
                 }
