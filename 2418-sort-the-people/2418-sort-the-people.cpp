7740df2a3d07346b96b94680d645558d7fea44f5
BHARGAVI CHAUDHARY <bhargavichaudhary803@gmail.com>
2026-04-07 00:23:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        int n = names.size();
-        int i,j;
-        string temp; int moli;
-        for(i=0;i<n-1;i++){
-            int key = i;
-            for(j=i+1;j<n;j++){
-                if(heights[j]>heights[key]){
-                    key = j;
-                }
-            }
-
-        moli = heights[i];
-        heights[i] = heights[key];
-        heights[key] = moli;
-
-        temp = names[i];
-        names[i] = names[key];
-        names[key] = temp;
+        vector<pair<int,string>> v;
+        
+        for(int i = 0; i < names.size(); i++){
+            v.push_back({heights[i], names[i]});
         }
-        return names;
 
+        sort(v.rbegin(), v.rend());
+
+        for(int i = 0; i < names.size(); i++){
+            names[i] = v[i].second;
+        }
+
+        return names;
     }
 };
