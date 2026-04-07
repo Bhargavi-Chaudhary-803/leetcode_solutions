fbc8f5ef0cd519cd99cf3e772cfa5398f5b52106
BHARGAVI CHAUDHARY <bhargavichaudhary803@gmail.com>
2026-04-08 00:13:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,30 +2,12 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        for(int i = 0; i < n - 1; i++) {
-            for(int j = 0; j < n - 1 - i; j++) {
-                if(nums[j] > nums[j + 1]) {
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                }
-            }
+        int sum = n * (n + 1) / 2;
+        
+        for(int i = 0; i < n; i++) {
+            sum -= nums[i];
         }
         
-        int low = 0;
-        int high = n - 1;
-        int mid;
-
-        while(low <= high) {
-            mid = (low + high) / 2;
-
-            if(nums[mid] == mid) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-
-        return low;
+        return sum;
     }
 };
