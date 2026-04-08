7494116236c6d76c4caac57d60dc060c3bff6cfb
BHARGAVI CHAUDHARY <bhargavichaudhary803@gmail.com>
2026-04-09 00:25:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
-        for (int i=0; i <n-1; i++) {
-            if (nums[i] > nums[i + 1]) {
+        for (int i = 0; i<=n; i++) {
+            if (nums[i] > nums[i+1]) {
                 return i;
             }
         }
-        return n - 1;
+        return n-1;
     }
 };
