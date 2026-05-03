f32bf1f1631a00b9dc21b796b990a7ac9385e795
BHARGAVI CHAUDHARY <bhargavichaudhary803@gmail.com>
2026-05-04 00:28:54 +0530

    Time: 743 ms (12.11%), Space: 480.8 MB (15.91%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     string replaceWords(vector<string>& dictionary, string sentence) {
+        unordered_set<string> st(dictionary.begin(), dictionary.end());
+
         stringstream ss(sentence);
         string word, result = "";
 
         while (ss >> word) {
-            string best = word;
+            for (int i = 1; i <= word.length(); i++) {
+                string prefix = word.substr(0, i);
 
-            for (auto &root : dictionary) {
-                if (word.find(root) == 0) {
-                    if (root.length() < best.length()) {
-                        best = root;
-                    }
+                if (st.find(prefix) != st.end()) {
+                    word = prefix;
+                    break;
                 }
             }
-
-            result += best + " ";
+            result += word + " ";
         }
 
         result.pop_back();
