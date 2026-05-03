80fc602489bae2d93e6d5380ecf6d09c7cf51392
BHARGAVI CHAUDHARY <bhargavichaudhary803@gmail.com>
2026-05-04 00:23:10 +0530

    Time: 711 ms (20.5%), Space: 481 MB (14.74%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        unordered_set<string> st(dictionary.begin(), dictionary.end());
+        stringstream ss(sentence);
+        string word, result = "";
+
+        while(ss >> word) {
+            for (int i = 1; i <= word.length(); i++) {
+                string prefix = word.substr(0, i);
+                if(st.find(prefix) != st.end()) {
+                    word = prefix;
+                    break;
+                }
+            }
+            result += word + " ";
+        }
+        result.pop_back();
+        return result;
+    }
+};
