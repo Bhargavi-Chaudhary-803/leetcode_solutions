0455ee2ffe81d1e224b1adf9d2d195c385f47d0e
BHARGAVI CHAUDHARY <bhargavichaudhary803@gmail.com>
2026-05-14 01:32:41 +0530

    Time: 0 ms (100%), Space: 11.7 MB (47.18%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+
+        for(int i = n-1; i >= 0; i--) {
+            if(nums[i] == val) {
+                swap(nums[i], nums[n - 1]);
+                n--;
+            }
+        }
+
+        return n;
+    }
+};
