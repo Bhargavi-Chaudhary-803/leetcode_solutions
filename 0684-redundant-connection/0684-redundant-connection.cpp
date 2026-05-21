ce637e12d1ab6b017b9c001683cdb8961a55eb0b
BHARGAVI CHAUDHARY <bhargavichaudhary803@gmail.com>
2026-05-21 23:33:01 +0530

    Time: 6 ms (18.84%), Space: 13.2 MB (70.85%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -4,7 +4,7 @@ public:
 
     int find(int x)
     {
-        if(parent[x] < 0)
+        if(parent[x] == x)
             return x;
 
         return parent[x] = find(parent[x]);
@@ -14,9 +14,12 @@ public:
     {
         int n = edges.size();
 
-        parent.resize(n + 1, -1);
+        parent.resize(n + 1);
 
-        for(int i = 0; i < n; i++)
+        for(int i = 1; i <= n; i++)
+            parent[i] = i;
+
+        for(int i = 0; i < edges.size(); i++)
         {
             int u = edges[i][0];
             int v = edges[i][1];
@@ -27,15 +30,7 @@ public:
             if(pu == pv)
                 return edges[i];
 
-            if(parent[pu] > parent[pv])
-            {
-                int temp = pu;
-                pu = pv;
-                pv = temp;
-            }
-
-            parent[pu] += parent[pv];
-            parent[pv] = pu;
+            parent[pu] = pv;
         }
 
         return {};
