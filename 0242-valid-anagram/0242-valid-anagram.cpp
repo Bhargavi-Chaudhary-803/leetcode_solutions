ab756de91c3cc59e67182a9bdbbfc128233fecfe
BHARGAVI CHAUDHARY <bhargavichaudhary803@gmail.com>
2026-05-27 01:56:54 +0530

    Time: 7 ms (18.87%), Space: 9.6 MB (75.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) {
+
+        if(s.size() != t.size())
             return false;
-        }
 
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
-        return s == t;
+
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] != t[i]){
+                return false;
+            }
+        }
+
+        return true;
     }
 };
