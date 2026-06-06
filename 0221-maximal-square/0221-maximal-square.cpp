24a1f357a6e0d84aa8382191b5b779accfcf1251
BHARGAVI CHAUDHARY <bhargavichaudhary803@gmail.com>
2026-06-07 03:17:54 +0530

    Time: 5 ms (80.25%), Space: 27.5 MB (94.77%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -4,29 +4,27 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
 
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        int maxi = 0;
+        vector<int> dp(n + 1, 0);
+        int maxSide = 0;
+        int prev = 0; 
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 1; i <= m; i++) {
+            prev = 0;
 
-                if (matrix[i][j] == '1') {
+            for (int j = 1; j <= n; j++) {
+                int temp = dp[j]; 
 
-                    if (i == 0 || j == 0) {
-                        dp[i][j] = 1;
-                    } else {
-                        dp[i][j] = 1 + min({
-                            dp[i - 1][j],
-                            dp[i][j - 1],
-                            dp[i - 1][j - 1]
-                        });
-                    }
-
-                    maxi = max(maxi, dp[i][j]);
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[j] = 1 + min({dp[j], dp[j - 1], prev});
+                    maxSide = max(maxSide, dp[j]);
+                } else {
+                    dp[j] = 0;
                 }
+
+                prev = temp;
             }
         }
 
-        return maxi * maxi;
+        return maxSide * maxSide;
     }
 };
